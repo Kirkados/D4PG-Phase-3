92ee9aa94535a5176708fa8e06d1b036b56fd96b
Kirkados <khovell@gmail.com>
2021-01-15 16:43:41 -0500

    email notification

--- compute_canada_run.sh
+++ compute_canada_run.sh
@@ -2,8 +2,14 @@
 #SBATCH --ntasks=4          # number of processes
 #SBATCH --mem-per-cpu=4G    # memory
 #SBATCH --account=def-sulrich
-#SBATCH --time=0-00:01:00   # time (DD-HH:MM:SS)
+#SBATCH --time=0-00:05:00   # time (DD-HH:MM:SS)
 #SBATCH --output=%j.out  # %N for node name, %j for jobID
+#SBATCH --mail-user=khovell@gmail.com
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=REQUEUE
+#SBATCH --mail-type=ALL
 
 module load python/3.7.4
 module load scipy-stack
