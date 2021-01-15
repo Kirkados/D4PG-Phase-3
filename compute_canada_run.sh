3de14e0b8caf83067614c26c43099afed960a42b
Kirkados <khovell@gmail.com>
2021-01-15 07:36:40 -0500

    made requirements file for node-based virtual environment on compute canada

--- compute_canada_run.sh
+++ compute_canada_run.sh
@@ -4,4 +4,14 @@
 #SBATCH --account=def-sulrich
 #SBATCH --time=0-00:01:00   # time (DD-HH:MM:SS)
 #SBATCH --output=%N-%j.out  # %N for node name, %j for jobID
+
+module load python/3.7.4
+virtualenv --no-download $SLURM_TEMPDIR/env
+source $SLURM_TEMPDIR/env/bin/activate
+pip3 install --no-index --upgrade pip
+
+pip3 install --no-index -f requirements.txt
+pip3 install PyVirtualDisplay-2.0-py2.py3-none-any.whl
+pip3 install EasyProcess-0.3-py2.py3-none-any.whl
+
 python3 main.py
