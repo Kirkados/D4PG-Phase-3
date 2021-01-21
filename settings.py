7cda4732feb1bf760f964e4bbfacbc252bf9ba3b
Kirkados <khovell@gmail.com>
2021-01-21 17:06:41 -0500

    *cedar

--- settings.py
+++ settings.py
@@ -16,7 +16,7 @@ class Settings:
     ##### Run Settings #####
     ########################
 
-    RUN_NAME               = 'CC_graham_MULTINODE_endOnCollision_Spin'
+    RUN_NAME               = 'CC_cedar_MULTINODE_endOnCollision_Spin'
     RESUME_TRAINING        = False # If True, main.py must be run from a previous run's 'code' folder.
     ENVIRONMENT            = 'manipulator' # 'quad1' for Task 1 velocity; 'quad1_accel' for Task 1 accel; 'quad1_runway' for Task 2 accel
     AGENT                  = '' # '' for Task 1, '_runway' for runway experiment
