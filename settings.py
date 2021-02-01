850389e84aa0490ae54a26922c1819d90d2a7a59
Kirkados <khovell@gmail.com>
2021-02-01 12:55:52 -0500

    with spin on cedar

--- settings.py
+++ settings.py
@@ -15,7 +15,7 @@ class Settings:
     ##### Run Settings #####
     ########################
 
-    RUN_NAME               = 'endCollide_r0p1_-100-125_noSpin_cedar'
+    RUN_NAME               = 'endCollide_r0p1_-100-125_Spin_cedar'
     RESUME_TRAINING        = False # If True, main.py must be run from a previous run's 'code' folder.
     ENVIRONMENT            = 'manipulator' # 'quad1' for Task 1 velocity; 'quad1_accel' for Task 1 accel; 'quad1_runway' for Task 2 accel
     AGENT                  = '' # '' for Task 1, '_runway' for runway experiment
