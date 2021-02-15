79e32df9d47b5906fc5a3819f357121be59500e9
Kirkados <khovell@gmail.com>
2021-02-15 13:06:36 -0500

    niagara changes

--- compute_canada_run.sh
+++ compute_canada_run.sh
@@ -16,26 +16,33 @@ NIAGARA=true
 
 if $NIAGARA # set to True if using Niagara
 then
-   #module load NiaEnv
-   #module load python/3.7.9
-   module load CCEnv arch/avx512 # uncomment if on Niagara, comment otherwise
-   module load StdEnv/2020 # uncomment if on Niagara, comment otherwise
+   module load NiaEnv/2019b
+   module load python/3.7.9
+   module load gcc/8.3.0
+   #module load CCEnv arch/avx512 # uncomment if on Niagara, comment otherwise
+   #module load StdEnv/2020 # uncomment if on Niagara, comment otherwise
+else
+   module load python/3.7.7
+   module load scipy-stack
 fi
-module load python/3.7.7
-module load scipy-stack
 module load geos
 if $NIAGARA
 then
    virtualenv --no-download virtualEnv
    source virtualEnv/bin/activate
+   pip3 install --upgrade pip
+   pip3 install -r requirements.txt
+   pip3 install PyVirtualDisplay
+   
 else
    virtualenv --no-download $SLURM_TMPDIR/env   
    source $SLURM_TMPDIR/env/bin/activate
+   pip3 install --no-index --upgrade pip
+   pip3 install --no-index -r requirements.txt
+   pip3 install EasyProcess-0.3-py2.py3-none-any.whl
+   pip3 install PyVirtualDisplay-2.0-py2.py3-none-any.whl
 fi
-pip3 install --no-index --upgrade pip
-pip3 install --no-index -r requirements.txt
-pip3 install EasyProcess-0.3-py2.py3-none-any.whl
-pip3 install PyVirtualDisplay-2.0-py2.py3-none-any.whl
+
 
 tensorboard --logdir=$SCRATCH/D4PG-Phase-3/Tensorboard/Current/ --host=0.0.0.0 &
 python3 -u main.py
