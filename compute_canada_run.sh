57c346471b11c1519130d2854558ebafae89b4ac
Kirkados <khovell@gmail.com>
2021-02-17 17:52:10 -0500

    major changes. Python virtual environemnt is now in $HOME. Virtual environemnts are mantually setup ahead of the run

--- compute_canada_run.sh
+++ compute_canada_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --cpus-per-task=80   # number of CPUs requested. Beluga: 40; Niagara: 40 (80 for hyperthreading); Cedar: 32 (or 48); Graham: 32
+#SBATCH --cpus-per-task=48   # number of CPUs requested. Beluga: 40; Niagara: 40 (80 for hyperthreading); Cedar: 32 (or 48); Graham: 32
 #SBATCH --time=0-01:00:00   # time (DD-HH:MM:SS) (Max on Beluga/Cedar/Graham: 7 days; Niagara 1 day)
 #SBATCH --nodes=1
 ##SBATCH --mem-per-cpu=4G
@@ -12,38 +12,31 @@
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-type=REQUEUE
 #SBATCH --mail-type=ALL
-NIAGARA=true
 
-if $NIAGARA # set to True if using Niagara
-then
+##Niagara commands if fresh
    #module load NiaEnv/2019b
    #module load python/3.7.9
    #module load gcc/8.3.0
    #module load geos
-   test=0
-else
-   module load python/3.7.7
-   module load scipy-stack
-   module load geos
-fi
-
-if $NIAGARA
-then
-   #virtualenv --system-site-packages virtualEnv
-   source virtualEnv/bin/activate
+   #virtualenv --system-site-packages $HOME/env    
    #pip3 install --upgrade pip
    #pip3 install -r requirements.txt
    #pip3 install PyVirtualDisplay
-   test2=0
-else
-   virtualenv --no-download $SLURM_TMPDIR/env   
-   source $SLURM_TMPDIR/env/bin/activate
-   pip3 install --no-index --upgrade pip
-   pip3 install --no-index -r requirements.txt
-   pip3 install EasyProcess-0.3-py2.py3-none-any.whl
-   pip3 install PyVirtualDisplay-2.0-py2.py3-none-any.whl
-fi
+   
+## Other system commands if fresh
+   #module load StdEnv/2020 # if you haven't upgraded to 2020 as the default yet
+   #module load python/3.7.7
+   #module load scipy-stack
+   #module load geos
+   #virtualenv --no-download $HOME/env    
+   #pip3 install --no-index --upgrade pip
+   #pip3 install --no-index -r requirements.txt
+   #pip3 install EasyProcess-0.3-py2.py3-none-any.whl
+   #pip3 install PyVirtualDisplay-2.0-py2.py3-none-any.whl
+
+source $HOME/env/bin/activate
 
 
+# Submit this job from the D4PG Folder
 tensorboard --logdir=$SCRATCH/D4PG-Phase-3/Tensorboard/Current/ --host=0.0.0.0 &
 python3 -u main.py
