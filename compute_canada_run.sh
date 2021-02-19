ad8d8fe287cf144f193b01243bce729e69568075
Kirkados <khovell@gmail.com>
2021-02-19 08:45:49 -0500

    reverted to creating the virtual environment in the job script, and that environment is now created in $SCRATCH instead of $HOME. Hopefully this will increase the CPU-efficiency

--- compute_canada_run.sh
+++ compute_canada_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --cpus-per-task=48   # number of CPUs requested. Beluga: 40; Niagara: 40 (80 for hyperthreading); Cedar: 32 (or 48); Graham: 32
-#SBATCH --time=0-01:00:00   # time (DD-HH:MM:SS) (Max on Beluga/Cedar/Graham: 7 days; Niagara 1 day)
+#SBATCH --time=4-00:00:00   # time (DD-HH:MM:SS) (Max on Beluga/Cedar/Graham: 7 days; Niagara 1 day)
 #SBATCH --nodes=1
 ##SBATCH --mem-per-cpu=4G
 #SBATCH --mem=0 # take all memory
@@ -29,14 +29,14 @@
 module load python/3.7.7
 module load scipy-stack
 module load geos
-   #virtualenv --no-download $HOME/env 
-   #source $HOME/env/bin/activate   
-   #pip3 install --no-index --upgrade pip
-   #pip3 install --no-index -r requirements.txt
-   #pip3 install EasyProcess-0.3-py2.py3-none-any.whl
-   #pip3 install PyVirtualDisplay-2.0-py2.py3-none-any.whl
+virtualenv --no-download $SCRATCH/env 
+source $SCRATCH/env/bin/activate   
+pip3 install --no-index --upgrade pip
+pip3 install --no-index -r requirements.txt
+pip3 install EasyProcess-0.3-py2.py3-none-any.whl
+pip3 install PyVirtualDisplay-2.0-py2.py3-none-any.whl
 
-source $HOME/env/bin/activate
+source $SCRATCH/env/bin/activate
 
 
 # Submit this job from the D4PG Folder
