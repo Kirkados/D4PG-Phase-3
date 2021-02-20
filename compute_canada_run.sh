9bc3fe666c05baab89834692699e5e9d5c232678
Kirkados <khovell@gmail.com>
2021-02-20 14:59:50 -0500

    making it niagara friendly

--- compute_canada_run.sh
+++ compute_canada_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --cpus-per-task=48   # number of CPUs requested. Beluga: 40; Niagara: 40 (80 for hyperthreading); Cedar: 32 (or 48); Graham: 32
+#SBATCH --cpus-per-task=80   # number of CPUs requested. Beluga: 40; Niagara: 40 (80 for hyperthreading); Cedar: 32 (or 48); Graham: 32
 #SBATCH --time=0-01:00:00   # time (DD-HH:MM:SS) (Max on Beluga/Cedar/Graham: 7 days; Niagara 1 day)
 #SBATCH --nodes=1
 ##SBATCH --mem-per-cpu=4G
@@ -12,9 +12,9 @@
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-type=REQUEUE
 #SBATCH --mail-type=ALL
-niagara=false
+niagara=true
 
-if [niagara]
+if $niagara
 then
    module load CCEnv arch/avx512
    module load StdEnv/2018.3
@@ -26,7 +26,8 @@ then
    source $HOME/env/bin/activate   
    
    # Do below before running
-   #virtualenv --system-site-packages $HOME/env  
+   #virtualenv --system-site-packages $HOME/env 
+   #source $HOME/env/bin/activate     
    #pip3 install --upgrade pip
    #pip3 install -r requirements.txt
    #pip3 install PyVirtualDisplay
