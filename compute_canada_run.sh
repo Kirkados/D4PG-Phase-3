04a66f77bd3faaf9be5151df5ceecad68b042a4f
Kirkados <khovell@gmail.com>
2021-02-20 14:43:19 -0500

    changing tensorflow back to 1.13.1 and reverting to StdEnv/2018.3 since 2020 and TF1.15.0 led to bad CPU Efficiency

--- compute_canada_run.sh
+++ compute_canada_run.sh
@@ -12,37 +12,44 @@
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-type=REQUEUE
 #SBATCH --mail-type=ALL
+niagara=false
 
-##Niagara commands if fresh
-   #module load NiaEnv/2019b
-   #module load python/3.7.9
+if [niagara]
+then
+   module load CCEnv arch/avx512
+   module load StdEnv/2018.3
+   module load python/3.7.4
+   module load scipy-stack
    #module load gcc/8.3.0
-   #module load geos
-   #virtualenv --system-site-packages $HOME/env   
-   # source $HOME/env/bin/activate   
+   module load geos
+    
+   source $HOME/env/bin/activate   
+   
+   # Do below before running
+   #virtualenv --system-site-packages $HOME/env  
    #pip3 install --upgrade pip
    #pip3 install -r requirements.txt
    #pip3 install PyVirtualDisplay
+else
+   module load StdEnv/2018.3
+   module load python/3.7.4
+   module load scipy-stack
+   module load geos
+   virtualenv --no-download $SLURM_TMPDIR/env 
+   source $SLURM_TMPDIR/env/bin/activate   
+   pip3 install --no-index --upgrade pip
+   pip3 install --no-index -r requirements.txt
+   pip3 install EasyProcess-0.3-py2.py3-none-any.whl
+   pip3 install PyVirtualDisplay-2.0-py2.py3-none-any.whl
+fi
    
 # If you want StdEnv2018.3
-module load StdEnv/2018.3
-module load python/3.7.4
+
 
    
 ## Other system commands if fresh
    #module load StdEnv/2020 # if you haven't upgraded to 2020 as the default yet
 #module load python/3.7.7
-module load scipy-stack
-module load geos
-virtualenv --no-download $SLURM_TMPDIR/env 
-source $SLURM_TMPDIR/env/bin/activate   
-pip3 install --no-index --upgrade pip
-pip3 install --no-index -r requirements.txt
-pip3 install EasyProcess-0.3-py2.py3-none-any.whl
-pip3 install PyVirtualDisplay-2.0-py2.py3-none-any.whl
-
-#source $SLURM_TMPDIR/env/bin/activate
-
 
 # Submit this job from the D4PG Folder
 tensorboard --logdir=$SCRATCH/D4PG-Phase-3/Tensorboard/Current/ --host=0.0.0.0 &
