4088718affd7b85e35d07c3fb949f7984c1d3f79
Kirkados <khovell@gmail.com>
2021-02-22 15:20:55 -0500

    time trial niagara

--- compute_canada_run.sh
+++ compute_canada_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --cpus-per-task=40   # number of CPUs requested. Beluga: 40; Niagara: 40 (80 for hyperthreading); Cedar: 32 (or 48); Graham: 32
+#SBATCH --cpus-per-task=80   # number of CPUs requested. Beluga: 40; Niagara: 40 (80 for hyperthreading); Cedar: 32 (or 48); Graham: 32
 #SBATCH --time=0-01:00:00   # time (DD-HH:MM:SS) (Max on Beluga/Cedar/Graham: 7 days; Niagara 1 day)
 #SBATCH --nodes=1
 ##SBATCH --mem-per-cpu=4G
@@ -12,7 +12,7 @@
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-type=REQUEUE
 #SBATCH --mail-type=ALL
-niagara=false
+niagara=true
 
 if $niagara
 then
