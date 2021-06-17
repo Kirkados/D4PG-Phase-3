ef411715b159c41c5a2786065d0b294bb5feb877
Kirkados <khovell@gmail.com>
2021-06-17 12:54:41 -0400

    ...clarifying it's on beluga

--- settings.py
+++ settings.py
@@ -15,7 +15,7 @@ class Settings:
     ##### Run Settings #####
     ########################
 
-    RUN_NAME               = 'RevertAccelerationLimits_inertialAcceleration_noSpin_lowRandomization_highVel_rcdc'
+    RUN_NAME               = 'RevertAccelerationLimits_inertialAcceleration_noSpin_lowRandomization_highVel_beluga'
     RESUME_TRAINING        = False # If True, main.py must be run from a previous run's 'code' folder.
     ENVIRONMENT            = 'manipulator' # 'quad1' for Task 1 velocity; 'quad1_accel' for Task 1 accel; 'quad1_runway' for Task 2 accel
     AGENT                  = '' # '' for Task 1, '_runway' for runway experiment
