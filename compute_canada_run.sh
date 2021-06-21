533e79e44f8f217e16330319827f9241cb671d47
Kirkados <khovell@gmail.com>
2021-06-21 11:32:09 -0400

    removing old acceleration limits, checking if inertial commands can learn in a spinning target environment

--- compute_canada_run.sh
+++ compute_canada_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --cpus-per-task=40   # number of CPUs requested. Beluga: 40; Niagara: 40 (80 for hyperthreading); Cedar: 32 (or 48); Graham: 32
+#SBATCH --cpus-per-task=48   # number of CPUs requested. Beluga: 40; Niagara: 40 (80 for hyperthreading); Cedar: 32 (or 48); Graham: 32
 #SBATCH --time=4-00:00:00   # time (DD-HH:MM:SS) (Max on Beluga/Cedar/Graham: 7 days; Niagara 1 day)
 #SBATCH --nodes=1
 ##SBATCH --mem-per-cpu=4G
--- settings.py
+++ settings.py
@@ -15,7 +15,7 @@ class Settings:
     ##### Run Settings #####
     ########################
 
-    RUN_NAME               = 'RevertAccelerationLimits_SPIN_inertialAcceleration_lowRandomization_wrapped_beluga'
+    RUN_NAME               = 'inertialAcceleration_SPIN_lowRandomization_wrapped_cedar'
     RESUME_TRAINING        = False # If True, main.py must be run from a previous run's 'code' folder.
     ENVIRONMENT            = 'manipulator' # 'quad1' for Task 1 velocity; 'quad1_accel' for Task 1 accel; 'quad1_runway' for Task 2 accel
     AGENT                  = '' # '' for Task 1, '_runway' for runway experiment
