24936eed0817d4815444801a0fbaa40595dd365f
Kirkados <khovell@gmail.com>
2021-07-03 18:38:20 -0400

    reverting angular acceleration only

--- settings.py
+++ settings.py
@@ -15,7 +15,7 @@ class Settings:
     ##### Run Settings #####
     ########################
 
-    RUN_NAME               = 'sensitivityRevert_a025_alpha04_spin'
+    RUN_NAME               = 'sensitivityRevert_a015_alpha1_spin'
     RESUME_TRAINING        = False # If True, main.py must be run from a previous run's 'code' folder.
     ENVIRONMENT            = 'manipulator' # uncomment for regular training
     #ENVIRONMENT            = 'fixedICs' # uncomment this option for testing what would have occurred from given initial conditions (also set RESUME_TRAINING to True)
