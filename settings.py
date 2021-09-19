4dada36586b5bc9abe4c4931b0dd554233a0dc67
Kirkados <khovell@gmail.com>
2021-09-19 12:05:36 -0400

    getting ready for publication

--- settings.py
+++ settings.py
@@ -15,7 +15,7 @@ class Settings:
     ##### Run Settings #####
     ########################
 
-    RUN_NAME               = 'final_spin_12day_rcdc'
+    RUN_NAME               = 'spinning_target_12days_training'
     RESUME_TRAINING        = False # If True, main.py must be run from a previous run's 'code' folder.
     ENVIRONMENT            = 'manipulator' # uncomment for regular training
     #ENVIRONMENT            = 'fixedICs' # uncomment this option for testing what would have occurred from given initial conditions (also set RESUME_TRAINING to True)
